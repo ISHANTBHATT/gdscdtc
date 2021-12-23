7c6e624fbed70ea54cdf5354707a0f2884c50484
Devanshsati <thedevanshsati@gmail.com>
2021-12-23 12:35:25 +0530

    added files , update html css

---
+++ src/Components/AboutUs.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export const AboutUs = () => {
+    return (
+    <>
+      <div id="aboutus">
+        <div class="left">
+          <div class="aboutush3p">
+            <h3>About Us</h3>
+            <p>
+              GOOGLE DEVELOPER STUDENT CLUBS ARE UNIVERSITY BASED COMMUNITY GROUPS FOR STUDENTS INTERSTED IN GOOGLE DEVELOPER TECHNOLOGIES.STUDENTS FROM ALL UNDERGRADUATE OR GRADUATE PROGRAMS WITH AN INTEREST IN GROWING AS DEVELOPER ARE WELCOME BY JOINING A GDSC, STUDENTS GROW THEIR KNOWLEDGE IN A PEER-TO-PEER LEARNING ENVIRONMENT AND BUILD SOLUTIONS FOR LOCAL BUSINESSES AND THEIR COMMUNITY.
+            </p>
+          </div>
+          <button type="button" class="btn btn-danger" name="button">Join Us</button>
+          <div class="aboutussocial">
+            <h2>Socials</h2>
+            <span>
+              <div class="social">
+                <a href="mailto:" target="_blank"><i class="fas fa-at" aria-hidden="true"></i></a>
+                <a href="tel:"><i class="fas fa-mobile-alt" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/" target="_blank"><i class="fab fa-linkedin" aria-hidden="true"></i></a>
+                <a href="https://www.youtube.com/" target="_blank"><i class="fab fa-youtube" aria-hidden="true"></i></a>
+                <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+              </div>
+            </span>
+          </div>
+        </div>
+        <div class="right">
+          <div class="aboutusimg">
+            <img src="\images\compimg.png" alt="computerimageimg"></img>
+          </div>
+          <div class="aboutusimages">
+            <i class="fab fa-java"></i>
+            <i class="fab fa-js-square"></i>
+            <i class="fab fa-html5"></i>
+            <i class="fab fa-css3-alt"></i>
+            <i class="fab fa-android"></i>
+            <i class="fab fa-python"></i>
+            <i class="fab fa-react"></i>
+            <i class="fab fa-git"></i>
+          </div>
+        </div>
+      </div>
+    </>
+		)
+}
---
+++ src/Components/Aim.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const Aim = () => {
+    return (
+    <>
+      <div id="aim">
+        <div class="aimgif">
+          <img class="aimimggif" src="\images\rotatingbox.gif" alt="boxes"></img>
+        </div>
+        <div class="aimcontent">
+          <div class="aimh1">
+            <h1>Our Focus</h1>
+          </div>
+          <div class="aimp">
+            <p>
+              We think slightly out of the box, we believe
+              that a club's resources must not only be channeled into conducting
+              events but also to propagate learning and teaching , symbiotically.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+    )
+}
